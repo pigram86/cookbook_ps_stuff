dbf242a98255c1bb08a05dc503059cfffffb8656
pigram86 <todd@toddpigram.com>
2014-07-17 18:51:24 -0400

    initial Commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'ps_stuff'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures ps_stuff'
+long_description 'Installs/Configures ps_stuff'
+version          '0.1.0'
+depends          "windows"
+
